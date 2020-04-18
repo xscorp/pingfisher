a4dd6c7580bd56c69be2c902e49ded92b6a167ec
xscorp <37046662+xscorp@users.noreply.github.com>
2020-04-18 19:23:22 +0000

    Added support for ICMPv6

--- pingfisher.py
+++ pingfisher.py
@@ -2,6 +2,7 @@ import socket
 import struct
 import time
 from sys import exit
+import ipaddress
 debug = 0
 
 def debug_print(*msg):
@@ -12,7 +13,6 @@ def debug_print(*msg):
 def parse_ipv4_address(ip_address):
 	return ".".join(map(str , ip_address))
 
-icmp_counter = 0
 ping_counter = 0
 sock = socket.socket(socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(3))
 while True:
@@ -37,12 +37,28 @@ while True:
 				
 				#check if the protocol field inside IPv4 packet is ICMP
 				if packet_protocol == 1:
+					#check if the ICMP packet is a ping request or ping response
 					if ipv4_packet[header_length] == 8:
-						print("Ping [REQUEST] from {}".format(parse_ipv4_address(source_ip)))
+						print("Ping \033[93m[REQUEST]\033[00m from {}".format(parse_ipv4_address(source_ip)))
 					elif ipv4_packet[header_length] == 0:
-						debug_print("Ping [REPLY] from {}".format(parse_ipv4_address(source_ip)))
+						print("Ping \033[95m[RESPONSE]\033[00m from {}".format(parse_ipv4_address(source_ip)))
 					ping_counter = ping_counter + 1
-						
+				
+			#check if the packet is IPv6 packet			
+			elif socket.htons(protocol) == 56710:
+				print("[+]Detected IPv6 packet")
+				ipv6_paket = ethernet_frame[14:]
+				version_class_flowLabel , payload_length , next_header , hop_limit , source_ipv6 , destination_ipv6 = struct.unpack("! 4s H B B 16s 16s" , ipv6_paket[:40])
+				debug_print("Payload_length: " , payload_length)
+				debug_print("Next_header: " , next_header)
+				debug_print("Hop_limit: " , hop_limit)
+				debug_print("Source IPv6: " , ipaddress.IPv6Address(source_ipv6))
+				debug_print("Destination IPv6: " , ipaddress.IPv6Address(destination_ipv6))
+				
+				#check if it's an ICMPv6 packet
+				if next_header == 58:
+					print("Ping received from {}".format(ipaddress.IPv6Address(destination_ipv6)))
+					
 
 	except KeyboardInterrupt:
 		print("\n[+]Received {} packets".format(ping_counter))
