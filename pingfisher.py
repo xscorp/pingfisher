816edccd4f812312f9b0a40f99517cca0e6b765b
xscorp <37046662+xscorp@users.noreply.github.com>
2020-04-19 08:17:24 +0000

    added debug and all flag and colors

--- pingfisher.py
+++ pingfisher.py
@@ -6,14 +6,43 @@ from argparse import ArgumentParser
 
 #parse command line arguments
 parser = ArgumentParser(description="")
-parser.add_argument("--debug" , "-d" , help="Debug mode" , action="store_true" , default = False)
+parser.add_argument("--debug" , "-d" , help="Debug mode" , action = "store_true" , default = False)
+parser.add_argument("--all" , "-a" , help="Capture all packets(used with -d option)" , action = "store_true" , default = False)
 arguments = parser.parse_args()
 
 #function to print the debug information only when the debug flag is set
 def debug_print(*msg):
 	if arguments.debug:
 		for data in msg:
-			print(data)
+			print(data , end = " ")
+		print()
+
+PACKET_TAB = "\t"
+
+def print_ethernet_frame_info(destination_mac , source_mac , protocol):
+	debug_print("\033[91m====================================================\033[00m")
+	debug_print("[+]Received Ethernet frame")
+	debug_print("[+]Source mac:" , source_mac)
+	debug_print("[+]Destination mac:" , destination_mac)
+	debug_print("[+]Protocol:" , socket.htons(protocol))
+
+def print_ipv4_packet_info(header_length , ttl , packet_protocol , source_ip , destination_ip):
+	debug_print("\033[34m++++++++++++++++++++++++++++++++++++++++++++++++++++\033[00m")
+	debug_print(PACKET_TAB + "[+]Detected IPv4 packet")
+	debug_print(PACKET_TAB + "[+]Header Length:" , header_length)
+	debug_print(PACKET_TAB + "[+]TTL:" , ttl)
+	debug_print(PACKET_TAB + "[+]Protocol:" , packet_protocol)
+	debug_print(PACKET_TAB + "[+]Source IPv4:" , source_ip)
+	debug_print(PACKET_TAB + "[+]Destination IPv4:" , destination_ip)
+	
+def print_ipv6_packet_info(version_class_flowLabel , payload_length , next_header , hop_limit , source_ipv6 , destination_ipv6):
+	debug_print("\033[34m++++++++++++++++++++++++++++++++++++++++++++++++++++\033[00m")
+	debug_print(PACKET_TAB + "[+]Detected IPv6 packet")
+	debug_print(PACKET_TAB + "[+]Payload_length:" , payload_length)
+	debug_print(PACKET_TAB + "[+]Next_header:" , next_header)
+	debug_print(PACKET_TAB + "[+]Hop_limit:" , hop_limit)
+	debug_print(PACKET_TAB + "[+]Source IPv6:" , IPv6Address(source_ipv6))
+	debug_print(PACKET_TAB + "[+]Destination IPv6:" , IPv6Address(destination_ipv6))
 
 #create raw socket
 sock = socket.socket(socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(3))
@@ -27,25 +56,29 @@ while True:
 		ethernet_frame , addr = sock.recvfrom(2048)
 		
 		if ethernet_frame:
-			debug_print("====================================================")
-			debug_print("[+]Received Ethernet frame")
-			debug_print("Printing raw frame data:\n" , ethernet_frame)
 			destination_mac , source_mac , protocol = unpack("! 6s 6s H" , ethernet_frame[:14])
-			debug_print("[+]Source mac:" , source_mac)
-			debug_print("[+]Destination mac:" , destination_mac)
-			debug_print("[+]Protocol:" , socket.htons(protocol))
+			
+			#if "all" flag is set, then debug_print every frame
+			if arguments.all:
+				print_ethernet_frame_info(destination_mac , source_mac , protocol)
 
 			#check if the packet is IPv4 packet
 			if socket.htons(protocol) == 8:
-				debug_print("[+]Detected IPv4 packet")
 				ipv4_packet = ethernet_frame[14:]
 				version_header_length = ipv4_packet[0]
 				header_length = (version_header_length & 15) * 4
 				ttl , packet_protocol , source_ip , destination_ip = unpack("! 8x B B 2x 4s 4s" , ipv4_packet[:20])
-				debug_print("Protocol inside IPv4:" , packet_protocol)
+				
+				#if "all" flag is set, then debug_print every packet
+				if arguments.all:
+					print_ipv4_packet_info(header_length , ttl , packet_protocol , source_ip , destination_ip)
 				
 				#check if the protocol field inside IPv4 packet is ICMP
 				if packet_protocol == 1:
+					
+					#print ethernet frame and IPv4 packet details if debug is set
+					print_ethernet_frame_info(destination_mac , source_mac , protocol)
+					print_ipv4_packet_info(header_length , ttl , packet_protocol , source_ip , destination_ip)
 					
 					#check if the ICMP packet is a ping request
 					if ipv4_packet[header_length] == 8:
@@ -60,18 +93,19 @@ while True:
 				
 			#check if the packet is an IPv6 packet			
 			elif socket.htons(protocol) == 56710:
-				debug_print("====================================================")
-				debug_print("[+]Detected IPv6 packet")
 				ipv6_packet = ethernet_frame[14:]
 				version_class_flowLabel , payload_length , next_header , hop_limit , source_ipv6 , destination_ipv6 = unpack("! 4s H B B 16s 16s" , ipv6_packet[:40])
-				debug_print("[+]Payload_length:" , payload_length)
-				debug_print("[+]Next_header:" , next_header)
-				debug_print("[+]Hop_limit:" , hop_limit)
-				debug_print("[+]Source IPv6:" , IPv6Address(source_ipv6))
-				debug_print("[+]Destination IPv6:" , IPv6Address(destination_ipv6))
+				
+				#if "all" flag is set, then debug_print every packet
+				if arguments.all:
+					print_ipv6_packet_info(version_class_flowLabel , payload_length , next_header , hop_limit , source_ipv6 , destination_ipv6)
 				
 				#check if it's an ICMPv6 packet
 				if next_header == 58:
+					
+					#print ethernet frame and IPv6 packet info if debug is set
+					print_ethernet_frame_info(destination_mac , source_mac , protocol)
+					print_ipv6_packet_info(version_class_flowLabel , payload_length , next_header , hop_limit , source_ipv6 , destination_ipv6)
 					
 					#check if the ICMPv6 packet is a ping reqeust
 					if ipv6_packet[40] == 128:
